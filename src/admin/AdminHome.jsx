8c920c8ef9b1cd29d2b652a6ce1f42abfdfe7f9f
Laxman <2300090230@kluniversity.in>
2026-03-03 19:08:53 +0530

    project updated

---
+++ src/admin/AdminHome.jsx
@@ -0,0 +1,10 @@
+const AdminHome = () => {
+    return (
+        <div>
+            <h3>Admin Home Page</h3>
+        </div>
+    )
+}
+
+
+export default AdminHome
---
+++ src/admin/AdminLogout.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function AdminLogout() 
+{
+  return (
+    <div>AdminLogout</div>
+  )
+}
